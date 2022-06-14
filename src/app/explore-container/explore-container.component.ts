4c13d2433d155d0bcd0490edce40c218767a582a
alveshenrique <alveshenrique@ymail.com>
2022-06-14 10:50:50 -0300

    Now using Geolocation from cordova
    Changed the geolocation component to use cordova for Android app compatibility

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
 import { Location } from '../models/location';
 
 @Component({
@@ -11,7 +12,7 @@ export class ExploreContainerComponent implements OnInit {
 
   text: string = "Initial Text";
   userLocation: Location = new Location();
-  constructor() { }
+  constructor(private geolocation: Geolocation) { }
 
   ngOnInit() {}
 
@@ -20,16 +21,26 @@ export class ExploreContainerComponent implements OnInit {
   }
 
   askForGeolocationPermission() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((data) => {
-        this.userLocation.latitude = data.coords.latitude;
-        this.userLocation.longitude = data.coords.longitude;
-      },
-      (error) => {},
-      {enableHighAccuracy: true,
-      timeout: 5000,
-    maximumAge: 10000});
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition((data) => {
+    //     this.userLocation.latitude = data.coords.latitude;
+    //     this.userLocation.longitude = data.coords.longitude;
+    //   },
+    //   (error) => {},
+    //   {enableHighAccuracy: true,
+    //   timeout: 5000,
+    // maximumAge: 10000});
+    // }
+    if(this.geolocation) {
+      this.geolocation.getCurrentPosition().then(
+        (r) => {
+          this.userLocation.latitude = r.coords.latitude;
+          this.userLocation.longitude = r.coords.longitude;
+        }
+      ).catch(
+        (e) => console.log("Error when getting location: ", e)
+      )
     }
-  }
+    }
 
 }
